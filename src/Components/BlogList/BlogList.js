c66b19269c7d0c5c09e3956f5bab5a39fdf89964
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-09-15 23:31:15 +0100

    save

--- src/Components/BlogList/BlogList.js
+++ src/Components/BlogList/BlogList.js
@@ -8,7 +8,7 @@ const  BlogList = ({blogs, title, handDelete}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                      <p>Written by { blog.author }</p>
-                     <button onClick={() => handDelete(blog.delete)}>Delete blog</button>
+                     <button onClick={() => handDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
